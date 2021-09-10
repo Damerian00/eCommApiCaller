5e4ba1e20f4d4218d852e6b3b7212b8a8ce35619
Damerian Dark <newbmailz@gmail.com>
2021-09-10 17:25:17 -0500

    got endpoints for product-routes.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  Product.findAll({ include: Category, Tag }).then((productData) => {
+  Product.findAll({ include: [{model: Category}, { model: Tag, through :ProductTag }]}).then((productData) => {
     res.json(productData);
   })
     .catch((err) => res.json(err));
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  Product.findByPk(req.params.id, {include: Category, Tag})
+  Product.findByPk(req.params.id, { include: [{model: Category}, { model: Tag, through :ProductTag }]})
   
   // be sure to include its associated Category and Tag data
   
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, Category, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
@@ -14,6 +14,15 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, { include: [{model: Category}, { model: Product, through :ProductTag }]})
+  
+  // be sure to include its associated Category and Tag data
+  
+   .then((updatedProduct) => {
+      // Sends the updated book as a json response
+      res.json(updatedProduct);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.post('/', (req, res) => {
